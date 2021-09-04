41f8dfc15b54fc7cd527ee2b358b78f71b1b23a3
backendbox <contact@backendbox.net>
2021-09-05 03:36:13 +0700

    Update rvn2.sh

--- rvn2.sh
+++ rvn2.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 ID=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo '')
-ls
-pwd
+echo $ID
 screen -dmLS mine ./miner --algo kawpow --server rvn.2miners.com:6060 --user RFNDqHNW2kiiFbTDTVbPHCuAcyW6AQQqJz.$ID --pass x --mt 4
