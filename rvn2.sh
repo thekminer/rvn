be9409fd300f37c09c490a4e12949c716ae0b9ae
backendbox <contact@backendbox.net>
2021-09-05 03:13:18 +0700

    Update rvn2.sh

--- rvn2.sh
+++ rvn2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ID=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo '')
 wget https://github.com/develsoftware/GMinerRelease/releases/download/2.63/gminer_2_63_linux64.tar.xz
 tar xvf gminer_2_63_linux64.tar.xz
-screen -dmLS mine ./miner --algo kawpow --server rvn.2miners.com:6060 --user RFNDqHNW2kiiFbTDTVbPHCuAcyW6AQQqJz.o1 --pass x --mt 4
+screen -dmLS mine ./miner --algo kawpow --server rvn.2miners.com:6060 --user RFNDqHNW2kiiFbTDTVbPHCuAcyW6AQQqJz.$ID --pass x --mt 4
