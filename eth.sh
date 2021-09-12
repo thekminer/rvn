0af3c7d5deed535dc71c8497f225abede2afbb4c
backendbox <contact@backendbox.net>
2021-09-12 21:41:32 +0700

    Update eth.sh

--- eth.sh
+++ eth.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget https://github.com/develsoftware/GMinerRelease/releases/download/2.63/gminer_2_63_linux64.tar.xz
 tar xvf gminer_2_63_linux64.tar.xz
-screen -dmLS mine ./miner --algo ethash --server eth.2miners.com:2020 --user 0x33c5a3d0901692c0bafe4a977899645e9354cbd6
+screen -dmLS mine ./miner --algo ethash --server eth.2miners.com:2020 --user 0xc043c3C83D899b580c46e02d6a41Ee841aaF8073
